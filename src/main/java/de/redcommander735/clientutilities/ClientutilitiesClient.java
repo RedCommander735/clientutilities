838183fb7bf140f3d2bc490cd30df9b639375ed5
Jonathan Schmidt <uxepd@student.kit.edu>
2024-12-21 12:05:03 +0100

    Setup

---
+++ src/main/java/de/redcommander735/clientutilities/ClientutilitiesClient.java
@@ -0,0 +1,10 @@
+package de.redcommander735.clientutilities;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ClientutilitiesClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
