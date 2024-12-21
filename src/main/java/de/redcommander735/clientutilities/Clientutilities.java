d315e134418083b91c7f8282c6981a4bd6ea2d96
Jonathan Schmidt <uxepd@student.kit.edu>
2024-12-21 11:57:26 +0100

    Initial commit

---
+++ src/main/java/de/redcommander735/clientutilities/Clientutilities.java
@@ -0,0 +1,16 @@
+package de.redcommander735.clientutilities;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Clientutilities implements ModInitializer {
+	public static final String MOD_ID = "clientutilities";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
